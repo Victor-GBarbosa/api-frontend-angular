b0611266026351bd53890c91f3f97998dbbe837b
Victor Hugo <victor.gbarbosa05@gmail.com>
2025-10-02 23:02:27 -0300

    feat: Add header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Header } from './components/header/header';
 
 export const routes: Routes = [
   { path: '', component: Login },
   { path: 'register', component: Register },
+  { path: 'test', component: Header },
 ];
