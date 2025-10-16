587c4e6c5006766b2197e0ff98bff857a5ffb88f
Victor Hugo <victor.gbarbosa05@gmail.com>
2025-10-15 23:50:25 -0300

    feat: user card + UserRoleModels

--- src/app/components/user-card/user-card.ts
+++ src/app/components/user-card/user-card.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ɵInternalFormsSharedModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { UserRolesEnum } from '../../models/userRole.model';
 
 @Component({
   selector: 'app-user-card',
-  imports: [],
+  imports: [ɵInternalFormsSharedModule, ReactiveFormsModule],
   templateUrl: './user-card.html',
-  styleUrl: './user-card.scss'
+  styleUrl: './user-card.scss',
 })
-export class UserCard {
+export class UserCard implements OnInit {
+  roleSelector!: FormGroup;
+  @Input() userCardInfo!: userCardModel;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    // agora userCardInfo já foi definido pelo Angular
+    this.roleSelector = this.fb.group({
+      userRole: [this.userCardInfo?.role, [Validators.required]],
+    });
+  }
+}
+
+export interface userCardModel {
+  id?: number;
+  name: string;
+  email: string;
+  role: UserRolesEnum;
 }
---
+++ src/app/models/userRole.model.ts
@@ -0,0 +1,6 @@
+export enum UserRolesEnum {
+  USER = 1,
+  SELLER = 2,
+  ADMIN = 3,
+  MASTER = 4,
+}
